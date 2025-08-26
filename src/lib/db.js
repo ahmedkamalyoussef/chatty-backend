801871d4466902ac9a38d45d7a36c2cba9a2068b
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2025-08-27 01:49:12 +0300

    updating db config

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB =async()=>{
     try{
-        const connection = await mongoose.connect("mongodb://localhost:27017/chat_db")//(process.env.MONGODB_URI);
+        const connection = await mongoose.connect(process.env.MONGODB_URI);
         console.log('Database connected successfully:', connection.connection.host);
     }catch(error){
         console.log('Database connection failed:', error);
